742bbb63cd33bcc86746338944cf6f86d3f06d53
Septy <Septy@DESKTOP-QQKUF81>
2017-02-28 12:21:01 +0700

    Add project files.

---
+++ asp-mvc-1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using asp_mvc_1.Security;
+
+namespace asp_mvc_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+
+}
